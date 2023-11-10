f16510bbaafd163d2a2409e39bd66ff9f55ca33a
JamesAskelson <jamesaskelson630@gmail.com>
2023-11-10 17:46:05 -0600

    group-anagrams

---
+++ random-leetcode/group-anagrams/java.js
@@ -0,0 +1,16 @@
+var groupAnagrams = function(strs) {
+    if (strs.length === 1) return [strs]
+    let wordMap = new Map()
+
+    for (let word of strs) {
+        let sortedWord = word.split('').sort().join('')
+        if (!wordMap.has(sortedWord)){
+            wordMap.set(sortedWord, [word])
+        }else{
+            wordMap.get(sortedWord).push(word)
+        }
+    }
+
+    return [...wordMap.values()]
+};
+
