b1550c75f3a1338468ce08fc19bcc4c37a5b2ff6
JamesAskelson <jamesaskelson630@gmail.com>
2023-11-14 19:09:09 -0600

    prod-of-array-exc-self done

---
+++ random-leetcode/prod-of-array-exc-self/java.js
@@ -0,0 +1,14 @@
+var productExceptSelf = function(nums) {
+    let res = []
+    let left = 1;
+    let right = 1;
+    for(let i = 0; i < nums.length; i++){
+        res[i] = left;
+        left = left * nums[i]
+    }
+    for(let j = nums.length -1; j >= 0; j--){
+        res[j] *= right;
+        right = right * nums[j]
+    }
+    return res
+};
