59880e06a0d072f17bbcd01bad1347694557ceea
JamesAskelson <jamesaskelson630@gmail.com>
2023-11-24 14:06:31 -0600

    get-concat-js-py

---
+++ random-leetcode/get-concatenation/java.js
@@ -0,0 +1,9 @@
+var getConcatenation = function(nums) {
+    let res = [...nums, ...nums]
+    return res;
+};
+
+var getConcatenation = function(nums) {
+    let res = nums.concat(nums)
+    return res;
+}
---
+++ random-leetcode/get-concatenation/python.py
@@ -0,0 +1,11 @@
+def getConcatenation(self, nums: List[int]) -> List[int]:
+    res = [*nums, *nums]
+    return res
+
+def getConcatenation(self, nums: List[int]) -> List[int]:
+    res = nums + nums
+    return res
+
+def getConcatenation(self, nums: List[int]) -> List[int]:
+    d=nums*2
+    return d
