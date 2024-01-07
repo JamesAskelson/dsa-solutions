0d4a2053d2e5f1ea56e353b12bf801aa47bf7f29
JamesAskelson <jamesaskelson630@gmail.com>
2024-01-06 21:02:50 -0600

    new container with most water solution js

---
+++ day-1/array manipulation/container with most water/java1.js
@@ -0,0 +1,16 @@
+var maxArea = function(height) {
+    let max = 0;
+    let left = 0;
+    let right = height.length - 1;
+    while(left < right) {
+        let dist = right - left
+        let leftVal = height[left]
+        let rightVal = height[right]
+        let sum = (leftVal > rightVal) ? rightVal*dist : leftVal*dist
+        if(sum > max) {
+            max = sum
+        }
+        leftVal > rightVal ? right-- : left++
+    }
+    return max
+};
