6ac957b8664bdc757b2bc546a10fdfb0ff08276f
JamesAskelson <jamesaskelson630@gmail.com>
2024-01-17 17:55:54 -0600

    reverse-linked-list

---
+++ day-4/linked-list/reverse-linked-list/java.js
@@ -0,0 +1,20 @@
+var reverseList = function(head) {
+    let prev = null;
+
+    while(head !== null) {
+        // create a pointer that points at everything after head
+        const next = head.next; // [2, 3]
+
+        // make w/e after head null on the first run, and every other run it'll be w/e prev is
+        head.next = prev // [null]
+
+        // since it's now just the first val of head, make prev head so that it reverses itself
+        prev = head // [1]
+
+        // make head next so that you can use it for future loops as the next variable
+        head = next // [2, 3]
+
+    }
+
+    return prev
+};
