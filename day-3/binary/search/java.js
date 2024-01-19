8ac653a2c30e8a6f7ae4ec6f9d051bb5942d37e8
JamesAskelson <jamesaskelson630@gmail.com>
2024-01-19 15:02:10 -0600

    search-js-py

---
+++ day-3/binary/search/java.js
@@ -0,0 +1,16 @@
+var search = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+
+    while(left <= right) {
+        let mid = left + Math.floor((right - left) / 2)
+        if(nums[mid] === target){
+            return mid
+        } else if (nums[mid] > target) {
+            right--
+        } else {
+            left++
+        }
+    }
+    return -1
+};
