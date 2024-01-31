c8a1d7a50e838a3ccfcaac81fc6983a17894d8df
JamesAskelson <jamesaskelson630@gmail.com>
2024-01-31 16:46:34 -0600

    max-depth-iter

--- day-4/trees/maximum-depth-of-binary-tree/java.js
+++ day-4/trees/maximum-depth-of-binary-tree/java.js
@@ -11,18 +11,21 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    // If the current node is null, it contributes a depth of 0 to the depth calculation
-    if (root === null) {
-        return 0;
+    if(root === null) return []
+    let count = 0;
+    let queue = [root]
+    while(queue.length > 0) {
+        let size = queue.length;
+        for(let i = 0; i < size; i++){
+            let curr = queue.shift()
+            if(curr?.left){
+                queue.push(curr.left)
+            }
+            if(curr?.right){
+                queue.push(curr.right)
+            }
+        }
+        count++
     }
-
-    // Recursively calculate the depth of the left subtree
-    const leftDepth = maxDepth(root.left);
-
-    // Recursively calculate the depth of the right subtree
-    const rightDepth = maxDepth(root.right);
-
-    // The maximum depth of the current subtree is the maximum of the depths of its left and right subtrees, plus 1 for the current node
-    return Math.max(leftDepth, rightDepth) + 1;
+    return count
 };
-
