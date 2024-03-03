9b7739c6424557c02b2177a4c3a9175a559a354f
JamesAskelson <jamesaskelson630@gmail.com>
2024-03-02 20:51:59 -0600

    search-range-js

---
+++ day-3/binary/search-range/js.js
@@ -0,0 +1,35 @@
+var searchRange = function(nums, target) {
+
+    const binarySearch = (nums, target, isSearchingLeft) => {
+        let low = 0;
+        let high = nums.length - 1;
+        let index = -1;
+
+        while(low <= high){
+            let mid = Math.floor((low + high) / 2)
+
+            if(nums[mid] > target){
+                high = high - 1
+            } else if (nums[mid] < target){
+                low = low + 1
+            } else {
+                // we currently know that the current mid is the target
+                index = mid
+                if(isSearchingLeft){
+                    // if isSearchingLeft is true, you start looking to the left of the current,
+                    // which is the target
+                    high = mid - 1;
+                } else {
+                    // if isSearchingLeft is false, you start looking to the right of the current,
+                    // which is the target
+                    low = mid + 1
+                }
+            }
+        }
+        return index
+    }
+
+    let left = binarySearch(nums, target, true)
+    let right = binarySearch(nums, target, false)
+    return [left, right]
+};
