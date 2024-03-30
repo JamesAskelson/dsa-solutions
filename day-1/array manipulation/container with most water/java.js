49c55dc8e40a54e839b19c233628b04b1f0ab926
JamesAskelson <jamesaskelson630@gmail.com>
2024-03-30 16:38:21 -0500

    container-most-watter-retry-js-py

--- day-1/array manipulation/container with most water/java.js
+++ day-1/array manipulation/container with most water/java.js
@@ -17,3 +17,24 @@ var maxArea = function(height) {
 
 
 };
+
+
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1
+    let max = 0
+    while(left < right){
+        let smaller = Math.min(height[left], height[right])
+        let product = (right - left) * smaller
+        if(product > max){
+            max = product
+        }
+
+        if(smaller == height[left]){
+            left++
+        } else {
+            right--
+        }
+    }
+    return max
+};
--- day-1/array manipulation/container with most water/python.py
+++ day-1/array manipulation/container with most water/python.py
@@ -21,3 +21,25 @@ def maxArea(self, height: List[int]) -> int:
             else:
                 right -= 1
         return max
+
+def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max = 0
+        while left < right:
+            leftHeight = height[left]
+            rightHeight = height[right]
+            width = right - left
+
+            lowerHeight = min(leftHeight, rightHeight)
+
+            storedWater = width * lowerHeight
+
+            if storedWater > max:
+                max = storedWater
+
+            if lowerHeight == leftHeight:
+                left += 1
+            else:
+                right -= 1
+        return max
