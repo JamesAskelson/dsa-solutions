46c1fb3299d602de5dd702cb17efe15827fe5947
JamesAskelson <jamesaskelson630@gmail.com>
2024-04-12 19:52:59 -0500

    sort-colors-retry-js-py

--- day-2/Sorting-algos/sort-colors/java.js
+++ day-2/Sorting-algos/sort-colors/java.js
@@ -1,23 +1,36 @@
 let sortColors = (nums) => {
     let low = 0;
-    let high = nums.length - 1
+    let high = nums.length - 1;
     let i = 0;
-    while (i <= high) {
-        if(nums[i] === 0) {
-            let tmp = nums[i]
+    while(i <= high){
+        if(nums[i] === 0){
             nums[i] = nums[low]
-            nums[low] = tmp
-            low++;
-            i++;
-        } else if (nums[i] === 2) {
-            let tmp = nums[i]
+            nums[low] = 0
+            i++
+            low++
+        } else if (nums[i] === 2){
             nums[i] = nums[high]
-            nums[high] = tmp
-            high--;
+            nums[high] = 2
+            high--
         } else {
             i++
         }
     }
-
-    return nums
 }
+
+def sortColors(self, nums: List[int]) -> None:
+        low = 0
+        high = len(nums) - 1
+        i = 0
+        while i <= high:
+            if nums[i] == 0:
+                nums[i] = nums[low]
+                nums[low] = 0
+                low= low+1
+                i= i+1
+            elif nums[i] == 2:
+                nums[i] = nums[high]
+                nums[high] = 2
+                high = high-1
+            else:
+                i=i+1
