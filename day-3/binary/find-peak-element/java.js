a595db0bac5bc42caf558131661c6abc0c7324ee
JamesAskelson <jamesaskelson630@gmail.com>
2024-04-13 19:48:44 -0500

    findpeakele-retry-js-py

--- day-3/binary/find-peak-element/java.js
+++ day-3/binary/find-peak-element/java.js
@@ -24,3 +24,19 @@ var findPeakElement = function(nums) {
 };
 
 console.log(findPeakElement([2,1]))
+
+var findPeakElement = function(nums) {
+    let low = 0;
+    let high = nums.length - 1;
+    while(low <= high){
+        let mid = Math.floor((low+high) / 2)
+        if((mid === 0 || nums[mid] > nums[mid-1]) && (mid === nums.length - 1 || nums[mid] > nums[mid+1])){
+            return mid
+        } else if(nums[mid] > nums[mid+1]){
+            high = mid - 1
+        } else {
+            low = mid + 1
+        }
+
+    }
+}
