582266991342b64d8f0fb57183b1367892ea2ebf
JamesAskelson <jamesaskelson630@gmail.com>
2024-05-21 21:24:59 -0500

    remove-dupes-js-py

---
+++ random-leetcode/remove-dupes/js.js
@@ -0,0 +1,10 @@
+var removeDuplicates = function(nums) {
+    let i = 1;
+    for(let j = 1; j < nums.length; j++){
+        if (nums[j-1] !== nums[j]) {
+            nums[i] = nums[j]
+            i++
+        }
+    }
+    return i
+};
---
+++ random-leetcode/remove-dupes/py.py
@@ -0,0 +1,7 @@
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 1
+        for j in range(1, len(nums)):
+            if nums[j - 1] != nums[j]:
+                nums[i] = nums[j]
+                i=i+1
+        return i
