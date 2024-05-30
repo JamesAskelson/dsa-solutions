51b0dcd18ec26de96aa78d69b46cf0440a0778f5
JamesAskelson <jamesaskelson630@gmail.com>
2024-05-30 18:06:00 -0500

    two-sum-retry-js-py

--- random-leetcode/two-sum/js.js
+++ random-leetcode/two-sum/js.js
@@ -1,12 +1,11 @@
 var twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++) {
-      let currNum = nums[i]
-      for(let j = i+1; j < nums.length; j++) {
-        let nextNum = nums[j]
-        let numSum = currNum + nextNum
-        if(target === numSum) {
-          return [i, j]
-        }
+  let map = {}
+  for(let i = 0; i < nums.length; i++){
+      let diff = target - nums[i]
+      if(map[diff] !== undefined){
+          return [i, map[diff]]
+      } else {
+          map[nums[i]] = i
       }
-    }
-};
+  }
+}
--- random-leetcode/two-sum/python.py
+++ random-leetcode/two-sum/python.py
@@ -1,10 +1,8 @@
-def twoSum(nums, target):
-    res = []
-    for i in range(len(nums)):
-        num = nums[i]
-        for j in range(i+1, len(nums)):
-            num2 = nums[j]
-            sum = num + num2
-            if sum == target:
-                res = [i, j]
-        return res
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mapp = {}
+        for i in range(len(nums)):
+            difference = target - nums[i]
+            if difference in mapp:
+                return [mapp[difference], i]
+            else:
+                mapp[nums[i]] = i
