243dc805e26999cf6600165c7d8b72862639ab1c
JamesAskelson <jamesaskelson630@gmail.com>
2024-08-03 22:13:33 -0500

    merge-sort-arr-py

---
+++ random-leetcode/merge-sort-arr/py.py
@@ -0,0 +1,10 @@
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i = 0
+        while m < len(nums1):
+            nums1[m] = nums2[i]
+            m += 1
+            i += 1
+        return nums1.sort()
