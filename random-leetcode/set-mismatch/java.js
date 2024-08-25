2ed369345673ca5760ca9e6b16aaa082b6e0804f
JamesAskelson <jamesaskelson630@gmail.com>
2024-08-24 22:08:13 -0500

    set-mismatch-retry

--- random-leetcode/set-mismatch/java.js
+++ random-leetcode/set-mismatch/java.js
@@ -29,3 +29,35 @@ var findErrorNums = function(nums) {
         return [dupe, dupe-diff]
     }
 };
+
+var findErrorNums = function(nums) {
+    let i = 1;
+    let targetSum = 0;
+    let sum = 0;
+    while(i <= nums.length) {
+        targetSum += i
+        i++
+    }
+
+    let set = new Set()
+    let dupe = 0;
+
+    for(let j = 0; j < nums.length; j++){
+        let curr = nums[j]
+        if(set.has(curr)){
+            dupe = curr
+        } else {
+            set.add(curr)
+        }
+
+        sum += curr
+    }
+
+    if(targetSum > sum){
+        let diff = targetSum - sum
+        return [dupe, dupe+diff]
+    } else {
+        let diff = sum - targetSum
+        return [dupe, dupe-diff]
+    }
+};
