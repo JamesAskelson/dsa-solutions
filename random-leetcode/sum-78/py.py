520ac24292eddd5164eb338e952242aec6fb5493
JamesAskelson <jamesaskelson630@gmail.com>
2024-08-24 22:09:20 -0500

    sum78-retry

--- random-leetcode/sum-78/py.py
+++ random-leetcode/sum-78/py.py
@@ -10,3 +10,16 @@ def sum78(nums):
     elif inside == True and num == 8:
       inside = False
   return max_sum
+
+def sum78(nums):
+  max_total = 0
+  inside = False
+
+  for num in nums:
+    if num == 7:
+      inside = True
+    elif inside == False:
+      max_total  = max_total  + num
+    elif inside == True and num == 8:
+      inside = False
+  return max_total
