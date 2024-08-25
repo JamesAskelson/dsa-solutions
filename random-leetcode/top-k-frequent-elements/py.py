cbdfdd496ea0e47a9d7534df41db39c63cedba98
JamesAskelson <jamesaskelson630@gmail.com>
2024-08-24 22:10:34 -0500

    topKFrequent-retry

--- random-leetcode/top-k-frequent-elements/py.py
+++ random-leetcode/top-k-frequent-elements/py.py
@@ -15,3 +15,20 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
             res.append(countarr[i][0])
 
         return res
+
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = {}
+        res = []
+        for val in nums:
+            if val in counter:
+                counter[val] = counter[val] + 1
+            else:
+                counter[val] = 1
+        countarr = list(counter.items())
+        countarr.sort(key=lambda x: x[1], reverse=True)
+        print(countarr)
+
+        for i in range(0, k):
+            res.append(countarr[i][0])
+
+        return res
