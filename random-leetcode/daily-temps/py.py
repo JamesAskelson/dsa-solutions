c6bf657663718cfb849fb397fcb53b5cd57f436a
JamesAskelson <jamesaskelson630@gmail.com>
2024-09-14 18:24:20 -0500

    daily-temp-retry-py

--- random-leetcode/daily-temps/py.py
+++ random-leetcode/daily-temps/py.py
@@ -12,3 +12,18 @@ def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
 
             stack.append([temp, i])
         return res
+
+def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0 for x in range(len(temperatures))]
+        for i in range(len(temperatures)):
+            temp = temperatures[i]
+
+            while(len(stack) > 0 and stack[len(stack) - 1][0] < temp):
+                [te, index] = stack.pop()
+                diff = i - index
+                print(stack, te, index, diff)
+                res[index] = diff
+
+            stack.append([temp, i])
+        return res
