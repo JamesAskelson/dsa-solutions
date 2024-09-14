a80e380dce773ee3d178097e21f0526fd6432ae6
JamesAskelson <jamesaskelson630@gmail.com>
2024-09-14 18:23:42 -0500

    daily-temp-js-retry

--- random-leetcode/daily-temps/js.js
+++ random-leetcode/daily-temps/js.js
@@ -15,4 +15,24 @@ var dailyTemperatures = function(temperatures) {
         stack.push([temp, i])
     }
     return res
-}; 
+};
+
+var dailyTemperatures = function(temperatures) {
+    let res = new Array(temperatures.length).fill(0)
+    let stack = []
+    for(let i = 0; i < temperatures.length; i++){
+        let temp = temperatures[i]
+
+        // console.log(stack[stack.length - 1], temp)
+
+        while(stack.length > 0 && stack[stack.length - 1][0] < temp) {
+            // console.log(stack[stack.length - 1])
+            let [te, index] = stack.pop()
+            let nDays = i - index
+            res[index] = nDays
+        }
+        stack.push([temp, i])
+    }
+    return res
+};
+
