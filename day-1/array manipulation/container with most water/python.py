d1e5ac17b76baf6c4c4c2d28871d46a9b777e55a
JamesAskelson <jamesaskelson630@gmail.com>
2024-10-05 20:11:30 -0500

    max-area-retry-py

--- day-1/array manipulation/container with most water/python.py
+++ day-1/array manipulation/container with most water/python.py
@@ -13,3 +13,19 @@
             else:
                 high -= 1
         return most_water
+
+    def maxArea(self, height: List[int]) -> int:
+        low = 0
+        high = len(height) - 1
+        most_water = 0
+        while low < high:
+            dist = high - low
+            lower_val = min(height[low], height[high])
+            curr_max = lower_val*dist
+            if curr_max > most_water:
+                most_water = curr_max
+            if lower_val == height[low]:
+                low += 1
+            else:
+                high -= 1
+        return most_water
