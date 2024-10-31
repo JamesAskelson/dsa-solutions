5c4826ac032059ab920acf2b4a6fb4104e88080d
JamesAskelson <jamesaskelson630@gmail.com>
2024-10-31 18:24:37 -0500

    c# easy problem testing

---
+++ random-practice/testing/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.ReadLine();
+
+for(int i = 1; i <= 9; i++){
+    Console.WriteLine("{0} x {1} = {2}", number, i, i * number);
+}
