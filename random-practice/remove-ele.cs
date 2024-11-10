20b47e6dff025afbc53c3c922b4b34e228eda2ab
JamesAskelson <jamesaskelson630@gmail.com>
2024-11-09 23:32:47 -0600

    remove-ele-c#

---
+++ random-practice/remove-ele.cs
@@ -0,0 +1,10 @@
+    public int RemoveElement(int[] nums, int val) {
+        int j = 0;
+        for(int i = 0; i < nums.Length; i++){
+            if(nums[i] != val){
+                nums[j] = nums[i];
+                j += 1;
+            }
+        }
+        return j;
+    }
